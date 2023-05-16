62c8b10aa5d106beca8de3e39a6e95768d7b4fdb
Salome Eriksson <salome.eriksson@unibas.ch>
2023-05-16 13:48:40 +0200

    Got first version of table report to work.

--- experimentviewer.py
+++ experimentviewer.py
@@ -1,18 +1,31 @@
 import param
 import panel as pn
+import copy
 
 from scatter import Scatterplot
 from table import Tablereport
 
+class ReportViewer(param.Parameterized):
+    report=param.Selector()
+
+    def __init__(self, **params):
+        super().__init__(**params)
+        self.param.report.objects = [Scatterplot(name = "Scatterplot"),Tablereport(name = "Tablereport")]
+        self.report = self.param.report.objects[1]
+
+    def view(self):
+        if self.report is not None:
+            return pn.Column(pn.Param(self.param, name="", expand_button=False), self.report.view);
+        
+
 class ExperimentViewer(param.Parameterized):
     num_reports = param.Integer(default=0)
+    reports = param.List(precedence=-1)
 
-    def __init__(self, reports=[], **params):
+    def __init__(self, reports=[ReportViewer()], **params):
         # ~ print("ExperimentViewer Init")
         super().__init__(**params)
-        self.reports = []
-        for report in reports:
-            self.reports.append(pn.widgets.Select(options=[report]))
+        self.reports = reports
         self.num_reports = len(self.reports)
         # ~ print("ExperimentViewer Init end")
 
@@ -23,15 +36,14 @@ class ExperimentViewer(param.Parameterized):
             self.reports = self.reports[0:self.num_reports]
         elif self.num_reports > len(self.reports):
             for i in range(len(self.reports), self.num_reports):
-                report_types = [Scatterplot(name = "Scatterplot"),Tablereport(name = "Tablereport")]
-                self.reports.append(pn.widgets.Select(options=report_types))
+                self.reports.append(ReportViewer())
         # ~ print("ExperimentViewer update num reports end")
 
     def view(self):
         # ~ print("ExperimentViewer view")
         reports_view = pn.Row()
         for report in self.reports:
-            reports_view.append(pn.Column(report, report.value.view))
+            reports_view.append(report.view)
         # ~ print("ExperimentViewer view return")
         return pn.Column(pn.Param(self.param),reports_view)
 
--- table.py
+++ table.py
@@ -13,6 +13,9 @@ from experimentdata import ExperimentData
 
 class Tablereport(Report):
     attribute = param.Selector()
+    aggregator = param.Selector(["sum", "mean", "min", "max"])
+    height = param.Integer(1000)
+    width = param.Integer(1500)
     
     def __init__(self, **params):
         print("TableReport init")
@@ -24,7 +27,16 @@ class Tablereport(Report):
 
     #@param.depends('algorithm_selector', 'algorithm_selector.algorithm_pairs')
     def data_view(self):
-        pn.widgets.DataFrame(value=self.experiment_data.data, hierarchical=True, height=400).show()
+        if not self.experiment_data.data.empty:
+            mapping = dict()
+            for alg in self.experiment_data.algorithms:
+                mapping[str(alg)+"_"+str(self.attribute)] = str(alg)
+            data = self.experiment_data.data
+            selected_data = data[[x for x in data.columns.values if x in mapping.keys()]]
+            selected_data = selected_data.rename(columns=mapping)
+            return pn.widgets.DataFrame(value=selected_data, hierarchical=True, aggregators={'problem': self.aggregator},
+                                        width=self.width, height=self.height)
+
         
     #@param.depends('algorithm_selector')
     def param_view(self):
